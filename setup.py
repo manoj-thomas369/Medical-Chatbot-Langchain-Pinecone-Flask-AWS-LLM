d814421e981892a47f7bd1f9677a7cebd78365a8
manoj-thomas369 <manojthomas3690@gmail.com>
2025-09-03 01:11:41 +0530

    Requirements Done

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="medical_chatbot",
+    version="0.1.0",
+    author="Manoj Kanna T",
+    author_email="manojthomas3690@gmail.com",
+    description="A medical chatbot using LangChain, Pinecone, Flask, and Google Gemini",
+    packages=find_packages(),
+    install_requires=[],
+)
